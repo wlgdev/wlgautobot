1d370c8dc067dd903e1bb224f01a40444d4a457e
shevernitskiy <mahahuha@ya.ru>
2026-03-10 11:00:58 +0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ import { fillGamesSheet } from "./features/games-url-to-google-sheets.ts";
 import { fillRecordsSheet } from "./features/records-url-to-goolge-sheets.ts";
 import { app } from "./app.ts";
 
-// Deno.cron("shorts stats", "0 0 26 * *", async () => {
-//   await scheduleStats();
-// });
+Deno.cron("shorts stats", "0 0 26 * *", async () => {
+  await scheduleStats();
+});
 
-// Deno.cron("fill games sheet", "0 7 * * *", async () => {
-//   await fillGamesSheet();
-// });
+Deno.cron("fill games sheet", "0 7 * * *", async () => {
+  await fillGamesSheet();
+});
 
-// Deno.cron("fill records sheet", "10 7 * * *", async () => {
-//   await fillRecordsSheet();
-// });
+Deno.cron("fill records sheet", "10 7 * * *", async () => {
+  await fillRecordsSheet();
+});
 
 Deno.serve({
   port: 6969,
